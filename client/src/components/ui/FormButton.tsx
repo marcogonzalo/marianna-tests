10468456a613590cb85d26ee95c381fb648d124d
Marco Gonzalo Gómez Pérez <410890+marcogonzalo@users.noreply.github.com>
2025-01-26 21:04:51 +0100

    Improve FormButton styles

--- client/src/components/ui/FormButton.tsx
+++ client/src/components/ui/FormButton.tsx
@@ -10,15 +10,21 @@ export default function FormButton({
     size = 'md',
     className = '',
     children,
+    disabled,
     ...props
 }: ButtonProps) {
     const baseStyles =
         'inline-flex justify-center rounded-md border py-2 px-4 text-sm font-medium shadow-sm';
 
     const variantStyles = {
-        primary:
-            'btn-primary border-transparent bg-indigo-600 text-white hover:bg-indigo-700',
-        secondary: 'btn-secondary hover:text-white hover:bg-indigo-700',
+        primary: {
+            base: 'btn-primary border-transparent bg-indigo-600 text-white',
+            hover: 'hover:bg-indigo-700',
+        },
+        secondary: {
+            base: 'btn-secondary text-white bg-indigo-400',
+            hover: 'hover:bg-indigo-700',
+        },
     };
 
     const sizeStyles = {
@@ -27,7 +33,11 @@ export default function FormButton({
         lg: 'h-11 px-6 text-lg',
     };
 
-    const combinedClassName = `${baseStyles} ${variantStyles[variant]} ${sizeStyles[size]} ${className}`;
+    const combinedClassName = `${baseStyles} ${variantStyles[variant].base} ${
+        !disabled && variantStyles[variant].hover
+    } ${sizeStyles[size]} ${
+        disabled ? 'opacity-50 cursor-not-allowed' : ''
+    } ${className}`;
 
     return (
         <Button className={combinedClassName} {...props}>
