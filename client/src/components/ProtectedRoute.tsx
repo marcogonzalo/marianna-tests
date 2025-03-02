35a07cae01d99e5225cf9f65b3d07f37a49c6f88
Marco Gonzalo Gómez Pérez <410890+marcogonzalo@users.noreply.github.com>
2025-03-02 17:28:14 +0100

    Fix missing session data when changing between routes

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,9 +1,17 @@
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '@/contexts/AuthContext';
+import { useEffect, useState } from 'react';
 
 export function ProtectedRoute({ children }: { children: React.ReactNode }) {
     const { token } = useAuth();
     const location = useLocation();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        setIsLoading(false);
+    }, [token]);
+
+    if (isLoading) return <></>;
 
     if (!token) {
         return <Navigate to="/login" state={{ from: location }} replace />;
