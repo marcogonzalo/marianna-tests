455260d7a21e3573348f418fb8abb26cd7773c22
Marco Gonzalo Gómez Pérez <410890+marcogonzalo@users.noreply.github.com>
2025-04-07 21:12:27 +0200

    CORS policy more flexible

--- api/main.py
+++ api/main.py
@@ -36,7 +36,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[CLIENT_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
